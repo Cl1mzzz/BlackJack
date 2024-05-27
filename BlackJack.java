94e64428346b9afc86fb2f12d763b3628de99c8b
Voyva <vborodiy2004@gmail.com>
2024-05-27 20:13:56 +0300

    Building the deck

--- BlackJack.java
+++ BlackJack.java
@@ -26,6 +26,17 @@ public class BlackJack {
     }
 
     public void buildDeck() {
+        deck = new ArrayList<Card>();
+        String[] values = {"A", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+        String[] type = {"C", "D", "H", "S"};
 
+        for(int i = 0; i < type.length; i++) {
+            for (int j = 0; j < values.length; j++) {
+                Card card = new Card(values[j], type[i]);
+                deck.add(card);
+            }
+        }
+        System.out.println("Колода карт:");
+        System.out.println(deck);
     }
 }
